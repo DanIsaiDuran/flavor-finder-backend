501d227c1c5795b060a38226825b59f1d1284ab3
DanIsaiDuran <dan.duran5635@alumnos.udg.mx>
2024-08-14 21:29:19 -0600

    First commit

---
+++ src/main/java/com/danduran/flavor_finder/FlavorFinderApplication.java
@@ -0,0 +1,13 @@
+package com.danduran.flavor_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorFinderApplication.class, args);
+	}
+
+}
