8bbeb3762618f638a6eb884e4218b54bb54add2d
DanIsaiDuran <dan.duran5635@alumnos.udg.mx>
2024-09-04 19:41:45 -0600

    feat: Role model created

---
+++ src/main/java/com/danduran/flavor_finder/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.danduran.flavor_finder.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.danduran.flavor_finder.model.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long>{
+
+}
