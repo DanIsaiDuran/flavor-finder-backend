0dcfef74db638f78ed26546d7b0ba1c40801e2fe
DanIsaiDuran <dan.duran5635@alumnos.udg.mx>
2024-09-18 18:02:28 -0600

    feat: query method created to find roles by list name

--- src/main/java/com/danduran/flavor_finder/repository/RoleRepository.java
+++ src/main/java/com/danduran/flavor_finder/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.danduran.flavor_finder.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.danduran.flavor_finder.model.Role;
+import java.util.List;
 
 public interface RoleRepository extends CrudRepository<Role, Long>{
 
+    List<Role> findRoleByNameIn(List<String> roleNames);
 }
