f62559a4334267bd6206514416d5fa49ebb6961c
Dan Duran <danisaidutru@gmail.com>
2025-03-12 18:52:19 -0600

    feat: JpaSpecification extended

--- src/main/java/com/danduran/flavor_finder/repository/RecipeRepository.java
+++ src/main/java/com/danduran/flavor_finder/repository/RecipeRepository.java
@@ -3,12 +3,13 @@ package com.danduran.flavor_finder.repository;
 import java.util.Optional;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import com.danduran.flavor_finder.model.Recipe;
 
 
-public interface RecipeRepository extends CrudRepository<Recipe, Long>{
+public interface RecipeRepository extends CrudRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe>{
     Optional<Recipe> findRecipeByName(String name);
     List<Recipe> findByUserId(Long userId);
     List<Recipe> findTop6ByOrderByIdAsc();
