1fe491064bb063e8d6f4bc2484a5be9ce87c66f8
Dan Duran <danisaidutru@gmail.com>
2025-05-06 20:01:05 -0600

    feat: Logged user is matched to its created recipe

--- src/main/java/com/danduran/flavor_finder/service/RecipeService.java
+++ src/main/java/com/danduran/flavor_finder/service/RecipeService.java
@@ -7,7 +7,7 @@ import com.danduran.flavor_finder.exception.UserNotFoundException;
 import com.danduran.flavor_finder.model.Recipe;
 
 public interface RecipeService {
-    Recipe createRecipe(Recipe recipe, Long id) throws UserNotFoundException;
+    Recipe createRecipe(Recipe recipe) throws UserNotFoundException;
     Recipe getRecipe(Long id) throws RecipeNotFoundException;
     void deleteRecipe(Long id);
     Recipe updateRecipe(Recipe recipe, Long id) throws RecipeNotFoundException;
